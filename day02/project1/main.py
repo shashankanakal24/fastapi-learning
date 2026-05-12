ab1f36f89e67485ef2f21d4b1ffcf657f8ed17dd
shashank24 <shashankanakal2004@gmail.com>
2026-05-12 18:29:35 +0530

    Day 2 of fastAPI

---
+++ day02/project1/main.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI,Path,HTTPException,Query
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open('patients.json','r') as f:
+        data = json.load(f)
+    return data
+
+@app.get("/")
+def hello():
+    return {'message':'Patient Management System API'}
+
+@app.get("/about")
+def about():
+    return {'message':' A fully funtional API to manage the your patients'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    
+    return data
+
+
+# @app.get('/patient/{patient_id}')
+# def view_patient(patient_id:str = Path(...,description="add the patient as in the db",example="P001")):
+#     data = load_data()
+    
+#     if patient_id in data:
+#         return data[patient_id]
+#     return {'error':'patient not found'}
+#the probkem with above code is even error status code still 200  to overcome it below i have utilized the https exception class 
+    
+    
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id:str = Path(...,description="add the patient as in the db",example="P001")):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404,detail="no patient found")
+
+
+@app.get("/sort")
+def sort_paients(sort_by:str=Query(...,description='sort on the basis of height , weight or bmi'),order:str =Query("asc",description="sort in asc or desc order")):
+    
+    valid_fields = ['height','weight','bmi']
+    
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f"Invalid field. Select from {valid_fields}")
+    
+    if order not in ['asc','desc']:
+        raise HTTPException(status_code=400,detail="Invalid order select between asc and desc")
+    data = load_data()
+    
+    sort_order = True if order =='desc' else False
+    
+    sorted_data=sorted(data.values(),key=lambda x:x.get(sort_by,0),reverse=sort_order)
+    
+    return sorted_data
+
+
+#http://127.0.0.1:8000/sort?sort_by=height&order=desc
+        
+    
